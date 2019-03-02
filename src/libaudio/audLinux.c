ce91f287b0dc1b3f647022d39ae1e38dc060769e
William Towle <william_towle@yahoo.co.uk>
2019-03-02 21:44:52 +0000

    Convert audio output to ALSA
    Changes:
    - Convert audLinux.c from OSS (/dev/dsp) use
    - Adapt build system to specify ALSA libraries
    - Default HAVE_FILEIO to off so audio I/O support builds by default

--- src/libaudio/audLinux.c
+++ src/libaudio/audLinux.c
@@ -1,13 +1,11 @@
 /* Ekaviel v1.54, Wm.Towle 25/06/1999 */
 
 #ifdef DEVEL
-#include <stdio.h>	/* FUTURE: remove! */
+#include <stdio.h>
 #endif
 
 #include <fcntl.h>
-#include <sys/ioctl.h>	/* linux/ioctl.h */
-#include <sys/soundcard.h>
-#include <unistd.h>	/* write() to a file descriptor */
+#include <alsa/asoundlib.h>
 #include "ekaudio.h"
 #include "ekmalloc.h"
 
@@ -19,25 +17,32 @@
 Flag	audioInit= FL_FALSE;
 int	outrate= DEFAULT_OUTRATE;
 
+#if 0	/* Obsolete: OS_Irix */
 static Flag	noWait;
+#endif
 static int	chans;
 static int	bleed1= 255,
 		bleed2= 0;
 static MSAMP	*outSamp= NULL;
 static int	buffPos;
 
+#if 0	/* Obsolete: Linux, OSS style */
 static int	audio= -1;		/* /dev/dsp */
 static int	dspSampSz= 16;		/* eight or sixteen bits */
+#else	/* ALSA */
+snd_pcm_t	*pcm_handle= NULL;
+snd_pcm_hw_params_t	*hw_params;
+#endif
+
 
 EkErrFlag audio_init(int hertz, Flag stereo, U_CHR bleed)
 {
-/* Linux requires we open /dev/dsp first... */
+#if 0	/* Obsolete: Linux, OSS style */
 #ifdef DEVEL
-fprintf(stderr, "opening /dev/dsp...\n");
+fprintf(stderr, "opening device...\n");
 #endif
 	if ( (audio= open("/dev/dsp", O_WRONLY, 0)) == -1 )
 	    return EK_ERR_NO_AUDIO;
-
 
 #ifdef DEVEL
 fprintf(stderr, "sound quality\n");
@@ -73,12 +78,37 @@ fprintf(stderr, "hertz\n");
 			48000 not widely supported (eg. SB!!!)
 			*/
 	    return EK_ERR_BAD_AUDCFG;
+#else
+#ifdef DEVEL
+fprintf(stderr, "opening PCM_DEVICE 'default'...\n");
+#endif
+	snd_pcm_open(&pcm_handle, "default", SND_PCM_STREAM_PLAYBACK, 0);
+	snd_pcm_hw_params_alloca(&hw_params);
+	snd_pcm_hw_params_any(pcm_handle, hw_params);
 
-/* Buffering characteristics... */
-/* FUTURE: Espie does 'SNDCTL_DSP_GETBLKSIZE' to make buffers */
+#ifdef DEVEL
+fprintf(stderr, "sound quality\n");
+#endif
+
+	/* Set parameters : interleaved channels, 16 bits little endian, 44100Hz, 2 channels */
+	snd_pcm_hw_params_set_access(pcm_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED);
+	snd_pcm_hw_params_set_format(pcm_handle, hw_params, SND_PCM_FORMAT_S16_LE);
+	snd_pcm_hw_params_set_rate_near(pcm_handle, hw_params, &outrate, 0);
+	snd_pcm_hw_params_set_channels(pcm_handle, hw_params, chans= stereo? 2 : 1);
+
+	/* Assign them to the playback handle and free the parameters structure */
+	snd_pcm_hw_params(pcm_handle, hw_params);
+	//snd_pcm_hw_params_free(hw_params);
+
+	/* Prepare & Play */
+	snd_pcm_prepare(pcm_handle);
+#endif
+
 	audioInit= FL_TRUE;
 	outrate= hertz;
+#if 0	/* Obsolete: OS_Irix */
 	noWait= FL_FALSE;
+#endif
 	chans= stereo? 2 : 1;
 /* set bleed (channel "overlap").....
  * Level 0:	normal stereo, left and right as expected
@@ -102,7 +132,7 @@ Flag audioBusy(int remain)
 #endif
 
 void getOutrate()
-{	/* Irix lets this change. Linux doesn't appear to. */	}
+{	/* Irix lets this change. Linux (OSS) doesn't appear to. */	}
 
 EkErrFlag audio_makeBuffers(int size)
 {
@@ -137,18 +167,26 @@ void audio_appendBuffers(int vchans, long left, long rght)
 /* For when audio is closed immediately afterward */
 void audio_discardBuffers()
 {
+#if 0	/* Obsolete: OS_Irix */
 	noWait= FL_TRUE;
+#endif
 }
 
 void audio_flushBuffers(Flag synchro)
-{	/* "audio" is the result from open() /dev/dsp */
+{
+#if 0	/* Obsolete: Linux, OSS style */
 	write(audio, outSamp, buffPos);
+#else
+	/* buf should be '[signed] short' type */
+	//if ((err = snd_pcm_writei(playback_handle, buf, 128)) != 128) {
+	snd_pcm_writei(pcm_handle, outSamp, buffPos);
+#endif
 #if 1
 #ifdef DEVEL
 	if (synchro)
 	    fprintf(stderr, "FUTURE: FIXME. 'synchro' not supported\n");
 #endif
-#else
+#else	/* OS_Irix */
 	if (synchro)
 	    while (audioBusy(buffPos)) sginap(1);
 #endif
@@ -159,11 +197,10 @@ void audio_close()
 	if (audioInit)
 	{
 #if 1
-#ifdef DEVEL
-	    if (!noWait)
-		fprintf(stderr, "FUTURE: FIXME: 'wait' not supported\n");
-#endif
-#else
+	    snd_pcm_drain(pcm_handle);
+	    snd_pcm_close(pcm_handle);
+	    /* ?and snd_pcm_hw_params_free(hw_params); */
+#else	/* Obsolete: OS_Irix */
 	    if (!noWait)
 		while (audioBusy(0)) sginap(1);
 #endif
@@ -174,5 +211,7 @@ void audio_close()
 	    FREE_MEMORY(outSamp, "ekaudio.c::audio_close()");
 	    outSamp= NULL;
 	}
+#if 0	/* Obsolete: OS_Irix */
 	noWait= FL_FALSE;	/* in case we open audio again */
+#endif
 }
